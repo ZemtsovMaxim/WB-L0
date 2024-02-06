34d1d04aba4b16d91cd64bfda8f5df2108fb7387
Max <124891621+ZemtsovMaxim@users.noreply.github.com>
2024-02-06 20:08:58 +0300

    Some changes

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -1,9 +1,13 @@
-\! ls -la
+RAISE NOTICE 'Creating database My_db';
 CREATE DATABASE My_db;
-\c My_db;
+
+-- Подключение к новой базе данных
+\connect My_db;
+
 CREATE USER My_user WITH PASSWORD '1234554321';
 GRANT ALL PRIVILEGES ON DATABASE My_db TO My_user;
 CREATE TABLE orders (
     order_uid VARCHAR PRIMARY KEY,
     data JSONB
 );
+
